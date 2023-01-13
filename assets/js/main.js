56303f6c350daa98e920cfb04b5297339971600e
saikiran12040 <saikirangen@gmail.com>
2023-01-13 15:58:29 +0530

    added files of responsive portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,67 @@ GitHubCalendar(".calendar", "saikiran12040");
          return fetch(`https://your-proxy.com/github?user=${saikiran12040}`)
        }
     }).then(r => r.text())
+
+
+    /*==========================scroll sections active link==================*/
+    const sections=document.querySelectorAll('section[id]')
+    function scrollActive(){
+        const scrollY=window.pageYOffset
+
+        sections.forEach(current=>{
+            const sectionHeight=current.offsetHeight
+            const sectionTop=current.offsetTop - 50;
+            sectionId=current.getAttribute('id')
+            if(scrollY>sectionTop && scrollY <= sectionTop+sectionHeight){
+                document.querySelector('.nav_menu a[href*='+sectionId+']').classList.add('active-link')
+            } else {
+                document.querySelector('.nav_menu a[href*='+sectionId+']').classList.remove('active-link')
+            }
+        })
+    }
+    window.addEventListener('scroll',scrollActive)
+
+    /*================change background header========*/
+    function scrollHeader(){
+        const nav=document.getElementById('header')
+        // when the scroll is greater than 200 viewport innerHeight,and the scroll-header class to the header tag
+        if(this.scrollY>=80)nav.classList.add('scroll-header');else nav.classList.remove('scroll-header')
+    }
+    window.addEventListener('scroll',scrollHeader)
+
+    /*==============show scroll up=============*/
+    function scrollUp(){
+        const scrollUp=document.getElementById('scroll-up');
+        // when the scroll is higher than 560 viewport height ,add the scroll show class
+        if(this.scrollY>=560)scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll')
+    }
+    window.addEventListener('scroll',scrollUp)
+
+    /*=============dark light theme===========*/
+    const themeButton=document.getElementById('theme-button')
+    const darkTheme='dark-theme'
+    const iconThme='uil-sum'
+
+    //previously selected topic(if user selected)
+    const selectedTheme=localStorage.getItem('seledted-theme')
+    const selectedIcon=localStorage.getItem('selected-icon')
+
+    //we obtain the current theme that the interface has by validating the dark-theme class
+    const getCurrentTheme=()=>document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+    const getCurrentIcon=()=>themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+    //we validate if the user previously chose a topic
+    if(selectedTheme){
+        //id thw validation is fulfilled ,we ask what the issue was to know if we activated 
+        document.body.classList[selectedTheme==='dark' ? 'add' :'remove'](darkTheme)
+        themeButton.classList[selectedIcon==='uil-moon' ? 'add' : 'remove'](iconTheme)
+    }
+    //activate/deactivate the theme manually with the button
+    themeButton.addEventListener('click',()=>{
+        //add or remove the dark/icon theme
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+        //we save the theme and the current icon that the user choes
+        localStorage.setItem('selected-theme',getCurrentTheme())
+        localStorage.setItem('selected-icon',getCurrentIcon())
+    })
